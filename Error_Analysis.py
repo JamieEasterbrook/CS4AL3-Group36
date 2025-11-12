00ca6d52a5b3c76f3252b5ac8da3cdefb915cbdd
JamieEasterbrook <easterbj@mcmaster.ca>
2025-11-11 19:37:49 -0500

    Added several new analysis features

---
+++ Error_Analysis.py
@@ -0,0 +1,125 @@
+import glob
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from fontTools.misc.cython import returns
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+
+from main import RecurrentNeuralNetwork
+import datetime as dt
+
+# Helper function to clean up data
+def clean_data_split(x,y,model,threshold=0.5,test_size=1, scalar=1):
+
+    # takes the most recent 100*n% of the dataset for testing
+    split_index = int(len(x) * (1 - test_size))
+    x_train = x[:split_index]; y_train = y[:split_index]
+    x_val = x[split_index:]; y_val = y[split_index:]
+
+    y_pred = [model(t.unsqueeze(0)) for t in x_val]
+    for t in y_pred:
+        t[t > threshold] = scalar;t[t < threshold] = 0
+    y_val = [t * scalar for t in y_val]
+
+    y_pred = np.array(y_pred).reshape(np.shape(y_val))
+    return x_val, y_val, x_train,y_train, y_pred
+
+
+def RMS_val (model,x,y,columns, feedback = False):
+    """
+    While these metrics are from a different dataset, the approach should still be the determining factor, so we will compare.
+    Our metrics to compare, from https://doi.org/10.1016/j.buildenv.2021.107601 are as follows:
+    Base airport data: MSE = 19.66
+    We want to beat this score to prove that our model surpassed a physics-based approach.
+
+    Feed-forward neural network: MSE = 31.28
+    This was a basic non-recurrent implementation, and should be our absolute base case to determine if the model is good or not.
+
+    Recurrent neural network: MSE = 4.72
+    This is the RNN that the paper implemented, and our target goal for this project. Approaching a MSE similar to this will show
+    whether the model can accurately predict weather values.
+
+    Gated Recurrent neural network: MSE = 2.96
+    This model uses a gated recurrent unit to help determine which information to forget, helping work in tomporal information better.
+    This model should be considered an upgrade to our current iteration, and while we are not expecting to surpass it, we
+    should try to aim close to this score anyways.
+    """
+
+    AIRPORT_MSE = 19.66
+    FNN_MSE = 31.28
+    RNN_MSE = 4.72
+    GRNN_MSE = 2.96
+
+    total_months = 147
+    x_val, y_val, x_train, y_train, y_pred = clean_data_split(x,y,model, threshold=0.33, test_size=1, scalar=len(columns))
+
+    MSE_list = np.square(np.array(y_val) - np.array(y_pred))
+    MSE = np.sqrt( np.sum(MSE_list)/len(x_val))
+
+    MSE_ZEROS = np.sqrt( np.sum(np.square(np.array(y_val) - np.zeros(np.shape(y_val))))/len(x_val))
+
+    if feedback:
+        if False:
+            plt.plot(range(len(x_val)), MSE_list)
+            plt.title(f'Per-point MSE over the last {total_months*test_size:.0f} months')
+            plt.xlabel('Date')
+            plt.ylabel('Mean Squared Error')
+            plt.show(block=False)
+            plt.pause(5)
+
+        print(f"{'-' * 16} MSE Comparison {'-' * 16}")
+        print(f"{'Model':<20} {'MSE':>10} {'Improvement':>16}")
+        print(f"{'-' * 48}")
+        print(f"{'Our RNN':<20} {MSE:>10.2f} {'-':>15}")
+        print(f"{'Zeros baseline':<20} {MSE_ZEROS:>10.2f} {AIRPORT_MSE / MSE * 100:>14.2f}%")
+        print(f"{'Airport':<20} {AIRPORT_MSE:>10.2f} {AIRPORT_MSE / MSE * 100:>14.2f}%")
+        print(f"{'FNN':<20} {FNN_MSE:>10.2f} {FNN_MSE / MSE * 100:>14.2f}%")
+        print(f"{'Baseline RNN':<20} {RNN_MSE:>10.2f} {RNN_MSE / MSE * 100:>14.2f}%")
+        print(f"{'Gated RNN':<20} {GRNN_MSE:>10.2f} {GRNN_MSE / MSE * 100:>14.2f}%")
+    return MSE
+
+
+# These are hard-coded lists of past validation loss functions, involving experiements with disabling different features
+base_i = [259, 518, 777, 1036, 1295, 1554, 1813, 2072, 2331, 2590, 2849, 3108, 3367, 3626, 3885, 4144, 4403, 4662, 4921, 5180, 5439, 5698, 5957, 6216, 6475, 6734, 6993, 7252, 7511, 7770, 8029, 8288, 8547, 8806, 9065]
+base_l = [0.8224111923883701, 0.7607846116197521, 0.7235216107861749, 0.7033979558739168, 0.7140400425113481, 0.7171712635919966, 0.7033634709900823, 0.7223344378430268, 0.7568681301741764, 0.7362939417362213, 0.7261025597309244, 0.7358390545022899, 0.7067187541517718, 0.7436778663561262, 0.6898356229066849, 0.7599375556255209, 0.7580448579171608, 0.7462144745834942, 0.7498617824809305, 0.817752114657698, 0.8248936811397816, 0.8114052990387226, 0.7927848357578804, 0.9521028438004954, 0.9482944972556213, 0.9662410239207333, 0.9536401526681308, 0.9955887152203198, 0.9341389115514427, 1.1328056593393456, 0.9501691434917778, 1.0619981509858165, 1.095580369234085, 1.0307816503376797, 1.0063344638409286]
+base_min = base_i[base_l.index(min(base_l))]
+
+nodrop_i = [259, 518, 777, 1036, 1295, 1554, 1813, 2072, 2331, 2590, 2849, 3108, 3367, 3626, 3885, 4144, 4403, 4662, 4921, 5180, 5439, 5698, 5957, 6216, 6475, 6734, 6993, 7252]
+nodrop_l = [0.8008562269909628, 0.7750300641717582, 0.7218175802765221, 0.7411371705860927, 0.7053549192075071, 0.709787091304516, 0.6869584352805697, 0.6793997822136715, 0.7214385779767201, 0.7177866445533161, 0.7316570970518835, 0.7589235593532694, 0.7284658607737772, 0.8203662042987758, 0.7951704053015545, 0.7994675723643139, 0.7415394197250235, 0.7886455557469664, 0.7725729377105318, 0.7987078965224069, 0.8756770745947443, 0.8357863719093388, 0.9734390940645645, 0.9079331031133389, 0.8315142909514492, 0.94678845256567, 0.9105629884991152, 1.0756002613182725]
+nodrop_min = nodrop_i[nodrop_l.index(min(nodrop_l))]
+
+nol2_i =  [259, 518, 777, 1036, 1295, 1554, 1813, 2072, 2331, 2590, 2849, 3108, 3367, 3626, 3885, 4144, 4403, 4662, 4921, 5180, 5439, 5698, 5957, 6216, 6475, 6734, 6993, 7252]
+nol2_l =  [0.8500936406439749, 0.7288552538074297, 0.7302758524130131, 0.7139199360691267, 0.7260159007434187, 0.7193152750360554, 0.6972454683534031, 0.6799354866660875, 0.7187511962035606, 0.7931359466807596, 0.7898478081514095, 0.830856090475773, 0.7859551659945784, 0.7514553779158099, 0.8350463494144637, 0.7983631239882831, 0.8715473449435728, 0.9326345722736984, 0.8353322245951357, 0.8741458867644442, 0.8976207249637308, 0.9211722835898399, 0.8185988495062138, 1.0169079974293709, 1.1746697292245667, 0.9863868653774261, 1.1248631605814243, 1.191721010567813]
+nol2_min = nol2_i[nol2_l.index(min(nol2_l))]
+
+
+noreg_i = [259, 518, 777, 1036, 1295, 1554, 1813, 2072, 2331, 2590, 2849, 3108, 3367, 3626, 3885, 4144, 4403, 4662, 4921, 5180, 5439, 5698, 5957, 6216, 6475, 6734, 6993, 7252, 7511, 7770, 8029]
+noreg_l = [0.8423401749339597, 0.7618973481244055, 0.7295128713394033, 0.7162640706218523, 0.7256219741599313, 0.736338006011371, 0.6856191199401329, 0.6969360668083717, 0.7610355055537718, 0.7495100683179395, 0.6928497259986812, 0.7840738291370457, 0.8560626835658632, 0.882247546623493, 0.83258292592805, 0.8609962643220507, 0.8805847250182053, 0.9868240145773723, 1.5275654258399174, 1.089889288719358, 1.0612542557305302, 1.0601208883112874, 1.095444596276201, 1.081957690674683, 1.1849492310449994, 1.0647290690705693, 1.1179841487058277, 1.3307801230714238, 1.177698676956111, 1.4519095014909218, 1.4804649527730613]
+noreg_min = noreg_i[noreg_l.index(min(noreg_l))]
+
+chop = 1
+
+plt.figure(figsize=(10, 6))
+plt.grid(True, alpha=0.3)
+plt.plot(base_i[:int(len(base_i)*chop)],base_l[:int(len(base_i)*chop)],label='l2 regularization and dropout layer', linestyle='-', color='green', marker='o')
+plt.plot(nodrop_i[:int(len(nodrop_i)*chop)],nodrop_l[:int(len(nodrop_l)*chop)],label='l2 regularization', linestyle='-', color='purple', marker='o')
+plt.plot(nol2_i[:int(len(nol2_i)*chop)],nol2_l[:int(len(nol2_l)*chop)],label='dropout layer', linestyle='-', color='red', marker='o')
+plt.plot(noreg_i[:int(len(noreg_i)*chop)],noreg_l[:int(len(noreg_l)*chop)],label='no regularization', linestyle='-', color='blue', marker='o')
+plt.axvline(base_min, color='green', linestyle='-', linewidth=2)
+plt.axvline(nodrop_min+25, color='purple', linestyle='-', linewidth=2)
+plt.axvline(nol2_min, color='red', linestyle='-', linewidth=2)
+plt.axvline(noreg_min, color='blue', linestyle='-', linewidth=2)
+plt.xlim(right=6000)
+plt.title('Comparizon of Different Regularization Methods on RNN')
+plt.legend()
+plt.savefig("report/Reg_Analysis.png", bbox_inches='tight')
+plt.show()
+
+import os
+print(os.getcwd())
--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import torch.optim as optim
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from torch.utils.data import Dataset, DataLoader
 
 # ###################################################### MODEL ######################################################
@@ -39,7 +41,7 @@ class RecurrentNeuralNetwork(nn.Module):
             self.hidden_size,
             self.output_size
         )
-        self.act = nn.Sigmoid()
+        self.act = nn.Identity()
 
     def forward(self, x: torch.Tensor):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
@@ -56,7 +58,7 @@ class SequencedDataset(Dataset):
 
     def __len__(self):
         return self.X.size(0) - self.seq_len
-    
+
     def __getitem__(self, idx):
         return self.X[idx:idx+self.seq_len], self.y[idx+self.seq_len]
 
@@ -70,49 +72,106 @@ def calculate_validation_loss(model: nn.Module, val: SequencedDataset, criterion
 
     with torch.no_grad():
         for X_batch, y_batch in val_loader:
-            pred: torch.Tensor = model(X_batch)
-            loss = criterion(pred, y_batch)
+            pred: torch.Tensor = model(X_batch.to(DEVICE))
+            loss = criterion(pred, y_batch.to(DEVICE))
             total_loss += loss.item()
         model.train()
     return total_loss / len(val_loader)
 
-def calculate_accuracy(model: nn.Module, val: SequencedDataset) -> float:
+def calculate_prediction(model: nn.Module, val_dataset: SequencedDataset) -> torch.Tensor:
     model.eval()
-    correct = 0
-    total = 0
-    val_loader = DataLoader(val, batch_size=1, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+    preds = []
     with torch.no_grad():
-        for X_batch, y_batch in val_loader:
-            pred: torch.Tensor = model(X_batch)
-            predicted = (pred >= 0.5).float()
-            correct += (predicted == y_batch).sum().item()
-            total += y_batch.numel()
-    return correct / total
+        for X_batch, _ in val_loader:
+            preds.append(torch.sigmoid(model(X_batch.to(DEVICE)))) # manually apply sigmoid since loss function does it now, loss isnt calculated here
 
-def load_data()->tuple[SequencedDataset, SequencedDataset]:
-    df = pd.read_csv('processed_data/final.csv')
-    X = torch.Tensor(df[FEATURE_COLUMNS].to_numpy())
-    y = torch.Tensor(df[TARGET_COLUMNS].to_numpy())
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, shuffle=False)
-    return SequencedDataset(X_train, y_train, SEQUENCE_LENGTH), SequencedDataset(X_val, y_val, SEQUENCE_LENGTH)
+    model.train()
+
+    return (torch.cat(preds) >= 0.5).float()
+
+# acc, recall, tpr
+def calculate_results(y_pred: torch.Tensor, y: torch.Tensor):
+    print(f'calculating accuracy, y_pred shape: {y_pred.size()}, y size: {y.size()}')
+    tp = ((y_pred==1) & (y==1)).sum().item()
+    tn = ((y_pred==0) & (y==0)).sum().item()
+    fp = ((y_pred==1) & (y==0)).sum().item()
+    fn = ((y_pred==0) & (y==1)).sum().item()
+    print(f'Counts: tp: {tp}, tn: {tn}, fp: {fp}, fn: {fn}')
+    total = y_pred.numel()
+    return (
+        (tp+tn)/total if total else 0,
+        tp/(tp+fn) if (tp+fn) else 0,
+        tp/(tp+fp) if (tp+fp) else 0
+    )
+
+def print_evaluation_metrics(model:nn.Module, val:SequencedDataset):
+    zeroed_y = torch.zeros(val.y.size()).to(DEVICE)
+    zeroed_acc, zeroed_recall, zeroed_prec = calculate_results(zeroed_y[val.seq_len:], val.y[val.seq_len:])
+    print(f'Baseline: Accuracy on all zero predictions: {zeroed_acc*100:.8f}%')
+    print(f'Baseline: Recall on all zero predictions: {zeroed_recall*100:.8f}%')
+    print(f'Baseline: Precision on all zero predictions: {zeroed_prec*100:.8f}%')
+    print(
+        f'Baseline: F1 score on all one predictions: {2 * (zeroed_prec * zeroed_recall) / (zeroed_prec + zeroed_recall + 1e-4) :.8f}')
+
+    print()
+
+    ones_y = torch.ones(val.y.size()).to(DEVICE)
+    ones_acc, ones_recall, ones_prec = calculate_results(ones_y[val.seq_len:], val.y[val.seq_len:])
+    print(f'Baseline: Accuracy on all one predictions: {ones_acc*100:.8f}%')
+    print(f'Baseline: Recall on all one predictions: {ones_recall*100:.8f}%')
+    print(f'Baseline: Precision on all one predictions: {ones_prec*100:.8f}%')
+    print(f'Baseline: F1 score on all one predictions: {2 * (ones_prec*ones_recall)/(ones_prec+ones_recall + 1e-4) :.8f}')
+
+    print()
+
+    flattened = [tuple(t.flatten().tolist()) for t in val.y]
+    from collections import Counter
+    most_common_flat = Counter(flattened).most_common(1)[0][0]
+    most_common_tensor = torch.tensor(most_common_flat).reshape(val.y[0].shape)
+    mcv = [most_common_tensor.clone() for _ in range(len(val.y))]
+    mcv = torch.stack(mcv).to(DEVICE)
+    mcv_acc, mcv_recall, mcv_prec = calculate_results(mcv[val.seq_len:], val.y[val.seq_len:])
+    print(mcv[0])
+    print(f'Baseline: Accuracy on most common vote predictions: {mcv_acc * 100:.8f}%')
+    print(f'Baseline: Recall on most common vote predictions: {mcv_recall * 100:.8f}%')
+    print(f'Baseline: Precision on most common vote predictions: {mcv_prec * 100:.8f}%')
+    print(f'Baseline: F1 score on most common vote predictions: {2 * (mcv_prec * mcv_recall) / (mcv_prec + mcv_recall + 1e-4) :.8f}')
+
+    print()
+
+    model_acc, model_recall,model_prec = calculate_results(calculate_prediction(model, val), val.y[val.seq_len:])
+    print(f'Model accuracy: {model_acc*100:.8f}%')
+    print(f'Model recall: {model_recall*100:.8f}%')
+    print(f'Model precision: {model_prec*100:.8f}%')
+    print(f'Model F1 score: {2 * (model_prec*model_recall)/(model_prec+model_recall + 1e-4) :.8f} ')
+
 
 
 # ###################################################### MAIN ######################################################
 
 # Hyperparameters
-LEARNING_RATE = 0.005
-DROPOUT_RATE = 0.0
+## Train
+LEARNING_RATE = 0.0001
 BATCH_SIZE = 32
-SEQUENCE_LENGTH = 30
-NUM_EPOCHS = 1
-HIDDEN_SIZE = 16
-NUM_LAYERS = 2
+SEQUENCE_LENGTH = 300
+NUM_EPOCHS = 9001
+HIDDEN_SIZE = 256
+NUM_LAYERS = 3
+## Regularize
+DROPOUT_RATE = 0.3
+L2_LAMBDA = 1e-03
 
-# Evaluation
-CHECK_EVERY = 1
+## Early Stop
+PATIENCE = 20
+THRESHOLD = 0.01
 
 # Configuration
 EVALUATION_MODE = True
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+### Used as a source path in evaluation mode and as a destination path in training mode, should be used to manage multiple models if needed
+MODEL_PATH_CUDA = 'model/rnn_model.pt'
+MODEL_PATH_CPU = 'model/rnn_model_cpu.pt'
 
 # Constants
 TARGET_COLUMNS = [
@@ -161,59 +220,97 @@ FEATURE_COLUMNS = [
     ]
 
 if __name__ == '__main__':
+    #print(torch.cuda.get_device_name())
     print('loading data...')
-    train_dataset, val_dataset = load_data()
+    df = pd.read_csv('processed_data/final.csv')
+    scalar = StandardScaler()
+    X = df[FEATURE_COLUMNS].to_numpy()
+    y = df[TARGET_COLUMNS].to_numpy()
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, shuffle=False)
+    X_train = scalar.fit_transform(X_train)
+    X_val = scalar.transform(X_val)
+
+    X_train = torch.Tensor(X_train).to(DEVICE)
+    X_val = torch.Tensor(X_val).to(DEVICE)
+    y_train = torch.Tensor(y_train).to(DEVICE)
+    y_val = torch.Tensor(y_val).to(DEVICE)
+
+    losses = []
+
+    train_dataset, val_dataset = SequencedDataset(X_train, y_train, SEQUENCE_LENGTH), SequencedDataset(X_val, y_val, SEQUENCE_LENGTH)
+
     print(f"Training Data:\n\tX:\n\t{train_dataset.X}\n\ty:\n\t{train_dataset.y}")
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    
+
     print('instantiating model...')
+
     model = RecurrentNeuralNetwork(
         input_size=len(FEATURE_COLUMNS),
         hidden_size=HIDDEN_SIZE,
         num_layers=NUM_LAYERS,
         output_size=len(TARGET_COLUMNS),
-        device=torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu'
-    )
-
+        device=DEVICE
+    ).to(DEVICE)
     if EVALUATION_MODE:
-        #model.load_state_dict(torch.load('model/rnn_model.pt'))
-        #accuracy = calculate_accuracy(model, val_dataset)
-        #print(f'Model accuracy on validation set: {accuracy*100:.2f}%')
+        # Evaluates function metrics
+        print('evaluating model...')
+        model.load_state_dict(torch.load(MODEL_PATH_CUDA, map_location=torch.device(DEVICE)))
+        model = model.to(DEVICE)
+        print_evaluation_metrics(model, val_dataset)
 
         model.eval()
-        from RMSE import RMS_val
+
+        from Error_Analysis import RMS_val, label_prediction
+
         x_val = [row[0] for row in val_dataset]
         y_val = [row[1] for row in val_dataset]
-        with torch.no_grad():
-            RMS_val(model,x_val,y_val,test_size = 0.5, feedback = True)
+        #with torch.no_grad():
+            #RMS_val(model, x_val, y_val,TARGET_COLUMNS, feedback=True)
 
         quit(0)
 
     model.train()
-    criterion = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    
+    pos_weight = (y_train.size(0)) / (y_train.sum(dim=0) + 1e-6) # prefers positive predictions, should be inversely proportional to how rare the class is
+    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.Tensor(pos_weight)).to(DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=L2_LAMBDA)
+
     print('starting training...')
     print(f'Max number of iterations: {len(train_loader) * NUM_EPOCHS}')
     iteration = 0
+    best_loss = float('inf')
+    best_model_state = None
+    num_no_improve = 0
     for epoch in range(NUM_EPOCHS):
         for X_batch, y_batch in train_loader:
-                inputs = X_batch
-                targets = y_batch
+            inputs = X_batch.to(DEVICE)
+            targets = y_batch.to(DEVICE)
 
-                optimizer.zero_grad()
-                preds = model(inputs)
+            optimizer.zero_grad()
+            preds = model(inputs)
 
-                loss = criterion(preds, targets)
+            loss = criterion(preds, targets)
+            loss.backward()
+            optimizer.step()
 
-                loss.backward()
-                optimizer.step()
+            iteration += 1
 
-                if (iteration+1) % CHECK_EVERY == 0:
-                    print(f"Iteration {iteration+1}, Loss: {calculate_validation_loss(model, val_dataset, criterion)}")
-                iteration += 1
+        current_loss = calculate_validation_loss(model, val_dataset, criterion)
+        losses.append(current_loss)
+        if current_loss - best_loss >= THRESHOLD:
+            num_no_improve += 1
+            if num_no_improve >= PATIENCE:
+                break
+        else:
+            num_no_improve = 0
+            if best_loss > current_loss:
+                best_loss = current_loss
+                best_model_state = copy.deepcopy(model.state_dict())
+        print(f"Iteration {iteration}, Loss: {current_loss}, No Improvement Count: {num_no_improve}")
 
-    print('training complete.')
+    es = 'due to early stop' if num_no_improve >= PATIENCE else ''
+    print(f'training complete {es}')
 
-    model_state = model.state_dict()
-    torch.save(model_state, 'model/rnn_model.pt')
+    torch.save(best_model_state, MODEL_PATH_CPU)
+
+    print(losses)
+    print(iteration)
